df2eaaa9bb41ea631db033893c22b523778c794f
Karma7580 <128285356+Karma7580@users.noreply.github.com>
2023-03-19 15:31:55 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Hello world");
+
+const buttonEl = document.querySelector('.button');
+            const blockEl = document.querySelector('.block');
+            buttonEl.addEventListener('click', function (e) {
+            blockEl.classList.toggle('visible');
+            });
